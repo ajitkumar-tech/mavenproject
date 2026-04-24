6a6afb3cebe40118929bdd0aa24914a1f5df6716
ajitkumar-tech <ajitsanjeevi229@gmail.com>
2026-04-24 21:38:49 +0530

    Update app.java

--- src/main/java/com.example/app.java
+++ src/main/java/com.example/app.java
@@ -1,1 +1,21 @@
 
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World from Java Application running in EKS 🚀");
+
+        while(true) {
+            try {
+                Thread.sleep(10000);
+                System.out.println("Application is running inside Kubernetes Pod...");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
